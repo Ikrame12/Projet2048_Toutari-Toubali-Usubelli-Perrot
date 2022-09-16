47e14a304e8ab221d2e72411015e8f4d95f9886e
Paulexpress <paulousubelli@gmail.com>
2022-09-16 11:51:35 +0200

    Création de la classe Parametres
    Les paramètres changeront au fur et à mesure du projet.

--- Projet2048_2022/src/projet2048_2022/Parametres.java
+++ Projet2048_2022/src/projet2048_2022/Parametres.java
@@ -10,5 +10,10 @@ package projet2048_2022;
  * @author ikram
  */
 public interface Parametres {
-    
+    static final int HAUT = 1;
+    static final int DROITE = 2;
+    static final int BAS = -1;
+    static final int GAUCHE = -2;
+    static final int TAILLE = 4;
+    static final int OBJECTIF = 2048;
 }
