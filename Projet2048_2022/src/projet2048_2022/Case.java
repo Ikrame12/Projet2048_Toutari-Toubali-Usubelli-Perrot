53bb6059db8e6072b5a67cd62cdc796fa1d86020
pauline789 <100521043+pauline789@users.noreply.github.com>
2022-09-20 16:35:23 +0200

    Création de la classe Case

--- Projet2048_2022/src/projet2048_2022/Case.java
+++ Projet2048_2022/src/projet2048_2022/Case.java
@@ -9,6 +9,120 @@ package projet2048_2022;
  *
  * @author ikram
  */
-public class Case {
-    
+    public class Case implements Parametres { 
+ private int x, y, valeur;    
+ private Grille grille;// on déclare une grille de type Grille en privée
+
+ //Constructeur de la classe "Case"
+ public Case(int abs, int ord, int v){
+ this.x=abs;
+ this.y=ord;
+ this.valeur=v;
+ }
+ 
+ //Création des setters et des getters
+ public void setGrille(Grille g){
+ this.grille=g;
+ }
+ 
+ public void setX(int x){
+ this.x=x;
+ }
+ 
+ public void setY(int y){
+ this.y=y;
+ }
+ 
+ public void setValeur(int valeur){
+ this.valeur=valeur;
+ }
+ 
+ public int getX(){
+ return this.x;
+ }
+ 
+ public int getY(){
+ return this.y;
+ }
+ 
+ public int getValeur(){
+ return this.valeur;
+ }
+
+     @Override
+public boolean equals(Object obj){
+    if(obj instanceof Case){
+    Case c= (Case) obj;
+    return (this.x==c.x && this.y==c.y);
+    }
+    else{
+        return false;
+    }
 }
+
+//commentaires 
+public int hashCode(){
+return this.x*7 + this.y*13;
+}
+ 
+//commentaires 
+ public boolean valeurEgale(Case c){
+ if (c != null){
+ return this.valeur ==c.valeur;
+ }else {
+ return false;
+ }
+ }
+ 
+ //comentaires
+ public Case getVoisinDirect(int direction){
+     if (direction == HAUT){
+     for(int i = this.y -1 ; i>=0; i--){
+       for (Case c : grille.getGrille()) {
+                    if (c.getX() == this.x && c.getY() == i){ 
+     return c;
+     }
+     }    
+     }
+     }
+     else if (direction == BAS){
+             for(int i= this.y +1; i<TAILLE; i++){
+             for (Case c : grille.getGrille()){
+             if(c.getX()== this.x && c.getY() == i){
+             return c;
+             }
+             }
+             }
+             }
+     else if (direction == GAUCHE){
+     for(int i = this.x -1; i>=0; i--){
+     for (Case c : grille.getGrille()){
+     if (c.getX()==i && c.getY()== this.y){
+     return c;
+     }
+     }
+     }
+     }
+     else if (direction == DROITE){
+     for (int i = this.x +1 ; i<TAILLE; i++){
+     for (Case c : grille.getGrille()){
+     if (c.getX()== i && c.getY() == this.y){
+     return c;
+             }
+     }
+     }
+     }
+ 
+ return null;
+ }
+ 
+ 
+ 
+ // Redéfinition de la méthode String
+ public String toString(){
+ return "Case("+this.x + "," +this.y +"," + this.valeur + ")";
+ }
+ 
+}
+
+
