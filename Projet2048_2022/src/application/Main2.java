c1e31f057317cea5862e6bf16ef2192bb6a45967
Ikrame12 <93207063+Ikrame12@users.noreply.github.com>
2022-12-09 15:46:43 +0100

    Interface graphique
    test pour voir si l'interface de scene builder est reliée à JAVA

--- Projet2048_2022/src/application/Main2.java
+++ Projet2048_2022/src/application/Main2.java
@@ -4,10 +4,13 @@
  */
 package application;
 
+import java.io.IOException;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -20,7 +23,7 @@ import javafx.stage.Stage;
 public class Main2 extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         Button btn = new Button();
         btn.setText("Start");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -30,6 +33,12 @@ public class Main2 extends Application {
                 System.out.println("Start");
             }
         });
+        
+        /*Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Scene scene = new Scene(root);
+        //boolean add = scene.getStylesheets().add("css/styles.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();*/ //je ne sais pas si ça marche
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
