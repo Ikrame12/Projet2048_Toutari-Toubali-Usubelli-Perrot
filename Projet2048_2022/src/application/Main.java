655a75d7a86cdc2df55b81bde428308c30c7eb2f
Hajartbl <93194552+Hajartbl@users.noreply.github.com>
2022-12-15 17:35:18 +0100

    Déplacement dans les 3 grilles

--- Projet2048_2022/src/application/Main.java
+++ Projet2048_2022/src/application/Main.java
@@ -1,19 +1,45 @@
 package application;
 
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.io.IOException;
+import java.util.Collection;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+  
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("interface1.fxml"));
+        
+        Scene scene = new Scene(root);
+        boolean add = scene.getStylesheets().add("css/styles.css");
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+   }
 
 
+   
 
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author hp
- */
-public class Main {
-
-}
 
